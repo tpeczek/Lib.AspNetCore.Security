eec25056e5baba4e0ff1f5b005c304e86a409df2
Tomasz Pęczek <tpeczek@gmail.com>
2018-02-20 23:43:28 +0100

    Adding AuthenticationService with Clear-Site-Data header support (#2)

---
+++ Lib.AspNetCore.Security/Authentication/ClearSiteDataAuthenticationService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
+using Lib.AspNetCore.Security.Http.Headers;
+using Lib.AspNetCore.Security.Http.Extensions;
+
+namespace Lib.AspNetCore.Security.Authentication
+{
+    internal class ClearSiteDataAuthenticationService : IAuthenticationService
+    {
+        #region Fields
+        private readonly IAuthenticationService _authenticationService;
+        private readonly ClearSiteDataHeaderValue _clearSiteDataHeaderValue;
+        #endregion
+
+        #region Constructor
+        public ClearSiteDataAuthenticationService(IAuthenticationService authenticationService, ClearSiteDataHeaderValue clearSiteDataHeaderValue)
+        {
+            _authenticationService = authenticationService ?? throw new ArgumentNullException(nameof(authenticationService));
+            _clearSiteDataHeaderValue = clearSiteDataHeaderValue ?? throw new ArgumentNullException(nameof(clearSiteDataHeaderValue));
+        }
+        #endregion
+
+        #region Methods
+        public Task<AuthenticateResult> AuthenticateAsync(HttpContext context, string scheme)
+        {
+            return _authenticationService.AuthenticateAsync(context, scheme);
+        }
+
+        public Task ChallengeAsync(HttpContext context, string scheme, AuthenticationProperties properties)
+        {
+            return _authenticationService.ChallengeAsync(context, scheme, properties);
+        }
+
+        public Task ForbidAsync(HttpContext context, string scheme, AuthenticationProperties properties)
+        {
+            return _authenticationService.ForbidAsync(context, scheme, properties);
+        }
+
+        public Task SignInAsync(HttpContext context, string scheme, ClaimsPrincipal principal, AuthenticationProperties properties)
+        {
+            return _authenticationService.SignInAsync(context, scheme, principal, properties);
+        }
+
+        public async Task SignOutAsync(HttpContext context, string scheme, AuthenticationProperties properties)
+        {
+            await _authenticationService.SignOutAsync(context, scheme, properties);
+
+            if (context.User?.Identity?.IsAuthenticated ?? false)
+            {
+                context.Response.SetClearSiteData(_clearSiteDataHeaderValue);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Lib.AspNetCore.Security/Authentication/ClearSiteDataAuthenticationServiceCollectionExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Authentication;
+using Lib.AspNetCore.Security.Authentication;
+using Lib.AspNetCore.Security.Http.Headers;
+
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extension methods for setting up authentication service which supports Clear-Site-Data header.
+    /// </summary>
+    public static class ClearSiteDataAuthenticationServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add authentication service which supports Clear-Site-Data header.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <param name="clearSiteDataHeaderValue">The Clear-Site-Data header value.</param>
+        /// <returns>The service collection.</returns>
+        public static IServiceCollection AddClearSiteDataAuthentication(this IServiceCollection services, ClearSiteDataHeaderValue clearSiteDataHeaderValue)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            ServiceDescriptor authenticationServiceDescriptor = services.FirstOrDefault(d => d.ServiceType == typeof(IAuthenticationService));
+            if (authenticationServiceDescriptor != null)
+            {
+                Type authenticationServiceImplementationType = authenticationServiceDescriptor.ImplementationType;
+                ServiceLifetime authenticationServiceLifetime = authenticationServiceDescriptor.Lifetime;
+
+                if (authenticationServiceImplementationType != null)
+                {
+                    services.Remove(authenticationServiceDescriptor);
+
+                    services.Add(new ServiceDescriptor(authenticationServiceImplementationType, authenticationServiceImplementationType, authenticationServiceLifetime));
+
+                    services.Add(new ServiceDescriptor(typeof(IAuthenticationService), (IServiceProvider serviceProvider) =>
+                    {
+                        IAuthenticationService authenticationService = (IAuthenticationService)serviceProvider.GetRequiredService(authenticationServiceImplementationType);
+
+                        return new ClearSiteDataAuthenticationService(authenticationService, clearSiteDataHeaderValue);
+                    }, authenticationServiceLifetime));
+                }
+            }
+
+            return services;
+        }
+    }
+}
